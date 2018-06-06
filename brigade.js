4f755ee097d810310a460443af5d5c9c9ab77c2b
JD Poligo <4595893+jdpoligo@users.noreply.github.com>
2018-06-06 23:42:56 +0000

    fix typo in brigade.js

--- brigade.js
+++ brigade.js
@@ -61,7 +61,7 @@ function kubeJobRunner (config, k) {
     k.image = "lachlanevenson/k8s-kubectl:v1.8.2"
     k.tasks = [
         `kubectl set image deployment/heroes-web-deploy heroes-web-cntnr=jdlabregistry.azurecr.io/azureworkshop/rating-web:${config.get("imageTag")}`
-    
+    ]    
 }
 
 function getBranch (p) {
